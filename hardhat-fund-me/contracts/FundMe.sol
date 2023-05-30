4331cc3822e178fc83e8647a92b5ee9914fc8f41
Creampanda <andrewfromtokyo@gmail.com>
2023-05-30 22:51:59 +0300

    Added some more tests

--- hardhat-fund-me/contracts/FundMe.sol
+++ hardhat-fund-me/contracts/FundMe.sol
@@ -52,7 +52,6 @@ contract FundMe {
             msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
             "You need to spend more ETH!"
         );
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
--- hardhat-fund-me/test/unit/FundMe.test.js
+++ hardhat-fund-me/test/unit/FundMe.test.js
@@ -69,5 +69,54 @@ describe("FundMe", function () {
                 endingDeployerBalance.add(gasCost).toString()
             );
         });
+        it("allows us to withdraw with multiple funders", async function () {
+            const accounts = await ethers.getSigners();
+            for (let i = 0; i < 6; i++) {
+                const fundMeConnectedContract = await fundMe.connect(
+                    accounts[i]
+                );
+                await fundMeConnectedContract.fund({ value: sendValue });
+            }
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            );
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            );
+
+            const transationResponse = await fundMe.withdraw();
+            const transationReceipt = await transationResponse.wait(1);
+            const { gasUsed, effectiveGasPrice } = transationReceipt;
+            const gasCost = gasUsed.mul(effectiveGasPrice);
+
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            );
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            );
+            assert.equal(endingFundMeBalance, 0);
+            assert.equal(
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            );
+
+            await expect(fundMe.funders(0)).to.be.reverted;
+
+            for (let i = 0; i < 6; i++) {
+                assert.equal(
+                    await fundMe.addressToAmountFunded(accounts[i].address),
+                    0
+                );
+            }
+        });
+        it("Only allows the owner to withdraw", async function () {
+            const accounts = await ethers.getSigners();
+            const attacker = accounts[1];
+            const attackerConnectedContract = await fundMe.connect(attacker);
+            await expect(
+                attackerConnectedContract.withdraw()
+            ).to.be.revertedWithCustomError(fundMe, "FundMe__NotOwner");
+        });
     });
 });
